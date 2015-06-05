0c2524f894bd2f80243804d60074d3330d8e14f3
Armin <hueneburg.armin@gmail.com>
2015-06-05 22:14:41 +0200

    integrated ibash.de and german-bash.org without breaking legacy code

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
+var args = process.argv;
+var options = [];
+if (args.length > 0) {
+    for (var i = 2; i < args.length; i++) {
+        if (args[i].indexOf('b') != -1) {
+            options.push('bash');
+        }
+        if (args[i].indexOf('g') != -1) {
+            options.push('german-bash');
+        }
+        if (args[i].indexOf('i') != -1) {
+            options.push('ibash');
+        }
+    }
+}
 
 var quote = require('./index');
 
-quote(function (err, quote) {
+quote(options, function (err, quote) {
   if (err) {
     throw err;
   }
--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 var request = require('request'),
     cheerio = require('cheerio');
 
-var REQUEST_URL = 'http://bash.org/?random1';
+
+var REQUEST_URL = '';
+var CLASS = '';
+var processQuote = {};
 
 
-module.exports = function (cb) {
+
+module.exports = function (options, cb) {
+    if (typeof options == "function") {
+        cb = options;
+        options = [0];
+    }
+    switch (options[Math.floor(options.length*Math.random())]) {
+        case 'bash':
+            REQUEST_URL = 'http://bash.org/?random1';
+            CLASS = '.qt';
+            processQuote = function(quote) { return quote.text().trim() };
+            break;
+
+        case 'german-bash':
+            REQUEST_URL = 'http://german-bash.org/action/random';
+            CLASS = '.zitat';
+            processQuote = function (quote) {
+                return quote.find('.quote_zeile').text().trim();
+            };
+            break;
+
+        case 'ibash':
+            REQUEST_URL = 'http://ibash.de/random.html';
+            CLASS = '.quote';
+            processQuote = function(quote, $) {
+                var q = '';
+                quote.find('.showcomment').remove();
+                quote.find('tr').each(function() {
+                    q += $(this).text() + '\n';
+                });
+                return q.trim();
+            };
+            break;
+    }
   request(REQUEST_URL, function (err, res, body) {
     if (err) {
       return cb(err);
@@ -17,8 +53,10 @@ module.exports = function (cb) {
 
     var quotes = [];
 
-    $('.qt').each(function () {
-      quotes.push($(this).text());
+    $(CLASS).each(function () {
+        var q = processQuote($(this), $);
+        if (q.length > 0)
+            quotes.push(q);
     });
 
     cb(null, quotes[Math.floor(quotes.length * Math.random())]);
